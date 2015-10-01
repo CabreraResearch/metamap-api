3414e054531e99b1e1141b201b22f33d14885ca8
Christopher Froehlich <crf@luddites.me>
2015-10-01 17:32:37 -0400

    First Auth0 api wrapper.

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 "use strict";
 // stackhut service
 let stackhut = require('./stackhut');
+const Auth0 = require('auth0')
+const Auth0Lock = require('auth0-lock')
 
 // create each service as either an ES6 class or an object of functions
-class Default extends stackhut.Service {
+class MetaMap extends stackhut.Service {
     constructor() {
         super();
-        // empty
+        this.lock = new Auth0Lock('VA5rA5k4JhlwPxq5ZISVMKpsQ0CSF4d1', 'metamap.auth0.com');
     }
 
-    add(x, y){
-        let res = x + y;
-        return Promise.resolve(res);
+    isAuthenticated(sessionId) {
+        let prms = new Promise((fulfill, reject) => {
+            this.lock.getProfile(sessionId, (err, profile) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    return fulfill(true)
+                }
+            });
+        });
+        return prms
     }
 }
 
 // export the services here
 module.exports = {
-    Default : new Default()
+    MetaMap: new MetaMap()
 };
