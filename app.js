1649f9032ca7429d8528df9cf9a1cc6dd8a28698
Christopher Froehlich <christopher@crlab.us>
2015-10-05 14:19:31 -0400

    Heroku

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
-"use strict";
-// stackhut service
-let stackhut = require('./stackhut');
+"use strict"
 
-// create each service as either an ES6 class or an object of functions
-class Default extends stackhut.Service {
-    constructor() {
-        super();
-        // empty
-    }
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-    add(x, y){
-        let res = x + y;
-        return Promise.resolve(res);
-    }
+app.use(bodyParser.json())       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({  // to support URL-encoded bodies
+  extended: true
+})) 
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
 }
 
-// export the services here
-module.exports = {
-    Default : new Default()
-};
+app.use('*', allowCrossDomain);
+
+app.use(function (req, res, next) {
+  console.log('Time: %d', Date.now());
+  next();
+})
+
+app.get('/', function (req, res) {
+  console.log('Request received: ' + req.url);
+  res.send('Hello World!');
+});
+
+app.post('/isAuthenticated', function (req, res) {
+  console.log('Request received: ' + req.url);
+  console.log(req.body);
+  
+  let Auth0 = require('auth0');
+  
+  Auth0.getUserInfo(
+    { 
+      domain: 'metamap.auth0.com',
+      userAccessToken: req.body.sessionId
+    },
+    (err, profile) => {
+      if (err) {
+          throw err;
+      } else {
+          res.send(true);
+      }
+  });
+  
+});
+
+let server = app.listen('8000');
